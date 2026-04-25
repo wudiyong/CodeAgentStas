4dae59fc0f2281aca8dda3f1f798f1ad01913d78
吴帝永 <774695423@qq.com>
2026-04-25 23:26:47 +0800

    Update CodeAgentStasApplicationTests.java - 2026-04-25 15:26:38

---
+++ src/test/java/com/example/codeagentstas/CodeAgentStasApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.codeagentstas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeAgentStasApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
